a64d0dfcf72306e4e74d5c4dfc23d31031a6044b
odkok <wilsonhung2002@gmail.com>
2021-05-12 00:15:01 +0800

    YouTubeHelper class

---
+++ helper.py
@@ -0,0 +1,212 @@
+import sys
+import os
+import subprocess
+
+try:
+    from pytube import YouTube
+    from pytube.helpers import safe_filename
+except ImportError:
+    print('[ERROR: dependencies not installed]')
+    print('[start installing dependencies by pip...]')
+    subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
+
+    from pytube import YouTube
+    from pytube.helpers import safe_filename
+
+try:
+    subprocess.run(['ffmpeg', '-version'], stdout=subprocess.DEVNULL)
+except FileNotFoundError:
+    FFMPEG_AVAILABLE = False
+else:
+    FFMPEG_AVAILABLE = True
+
+
+class YouTubeHelper:
+    def __init__(self, video_link):
+        """
+        initialize helper object and check video availability
+
+        :param video_link: link to the YouTube video
+        :type video_link: str
+        """
+        self.yt = YouTube(video_link)
+        self.yt.check_availability()  # throw error if not available
+
+    def auto_download(self, myfolder=None):
+        """
+        download highest quality video available in mp4 format
+
+        :param myfolder: directory of downloaded video
+        :type myfolder: str or path-like or None
+        :return: None
+        """
+        resolutions = self.get_all_resolution()
+        highest_resolution = None
+        fps = None
+        # find highest resolution and highest fps if possible
+        for res in resolutions:
+            i = res.find('p')
+            number = res[:i]
+            frame_per_second = res[i + 1:]
+            if highest_resolution and number < highest_resolution:
+                break
+            if not highest_resolution:
+                highest_resolution = number
+            if not fps and frame_per_second:
+                fps = int(frame_per_second)
+            elif frame_per_second and int(frame_per_second) > fps:
+                fps = int(frame_per_second)
+        highest_resolution = str(highest_resolution) + 'p'
+        conventional = highest_resolution + str(fps) if fps else highest_resolution
+        print('video with resolution {} will be downloaded'.format(conventional))
+        self.get_video(highest_resolution, myfolder=myfolder, fps=fps)
+
+    def get_video(self, resolution, myfolder=None, fps=None):
+        """
+        download video in mp4 format
+
+        :param myfolder: path to folder for downloaded video and audio
+        :type myfolder: path-like or str or None
+        :param resolution: resolution ends with 'p'
+        :type resolution: str
+        :param fps: frame per second
+        :type fps: int
+        :return: None
+        """
+        title = self.yt.title
+        valid_filename = safe_filename(title) + '.mp4'
+        file_path = os.path.join(myfolder, valid_filename) if myfolder else valid_filename
+        progressive_video = self.yt.streams.filter(progressive=True, resolution=resolution, fps=fps)
+        # download progressive video if possible
+        if progressive_video:
+            print("[Downloading...]")
+            progressive_video.last().download(output_path=myfolder, filename=title)
+            return
+        if not FFMPEG_AVAILABLE:
+            print(
+                '[ERROR: ffmpeg not found. Cannot perform downloading.'
+                'Make sure ffmpeg is installed and added in PATH]')
+            print('[PROGRAM TERMINATED]')
+            sys.exit()
+        # search for video with specific resolution and fps
+        video_search_result = self.yt.streams.filter(only_video=True, resolution=resolution, fps=fps)
+        if video_search_result:
+            print("[Downloading...]")
+            video_path = video_search_result.last().download(
+                output_path=myfolder, filename='video')
+            audio_path = self.yt.streams.filter(only_audio=True).order_by('abr').last().download(output_path=myfolder,
+                                                                                                 filename='audio')
+            subprocess.run(
+                ['ffmpeg', '-i', video_path, '-i', audio_path, '-vcodec', 'copy', '-acodec', 'aac', file_path])
+            os.remove(video_path)
+            os.remove(audio_path)
+        else:
+            quality = resolution + str(fps) if fps else resolution
+            raise ValueError("there is no video with quality {}".format(quality))
+
+    def get_audio(self, myfolder=None, quality=None, audio_format=None):
+        """
+        download audio in different format
+
+        :param myfolder: directory for downloading audio
+        :type myfolder: str or path-like or None
+        :param quality: bit rate
+        :type quality: int or None
+        :param audio_format: audio format e.g.mp3,w4a
+        :type: str
+        :return: None
+        """
+        filename = safe_filename(self.yt.title)
+        if quality is None:
+            target = self.yt.streams.filter(only_audio=True).order_by('abr').last()
+        elif not isinstance(quality, int):
+            raise TypeError("quality should be int")
+        else:
+            abr = str(quality) + 'kbps'
+            search_result = self.yt.streams.filter(only_audio=True, abr=abr)
+            if search_result:
+                target = search_result.last()
+            else:
+                raise ValueError("there is no audio with bit rate {}".format(abr))
+        print("audio with {} is going to be downloaded".format(target.abr))
+        print("[Downloading...]")
+        audio_path = target.download(output_path=myfolder, filename=filename)
+        print('[Download success]')
+        if audio_format:
+            audio_format = audio_format if audio_format[0] == '.' else '.' + audio_format
+            path, ext = os.path.splitext(audio_path)
+            if ext != audio_format:
+                if not FFMPEG_AVAILABLE:
+                    print(
+                        '[ERROR: ffmpeg not found. Cannot perform conversion.'
+                        'Make sure ffmpeg is installed and added in PATH]')
+                    print('[PROGRAM TERMINATED]')
+                    sys.exit()
+                output_path = os.path.join(myfolder, filename + audio_format) if myfolder else filename + audio_format
+                subprocess.run(['ffmpeg', '-i', audio_path, output_path])
+                os.remove(audio_path)
+
+    def get_all_resolution(self):
+        """
+        print all available resolution
+
+        :return: list of strings containing available resolution
+        :rtype: list[str]
+        """
+        resolutions = []
+        for stream in self.yt.streams.filter(only_video=True).order_by('resolution').desc():
+            res = stream.resolution
+            fps = stream.fps
+            if fps > 30:
+                res += str(fps)
+            if res in resolutions:
+                continue
+            else:
+                resolutions.append(res)
+        return resolutions
+
+    def get_all_audio_quality(self):
+        """
+        print all available bit rate
+
+        :return: list of string containing available bit rate in terms of kbps
+        :rtype: list[str]
+        """
+        audios = []
+        for stream in self.yt.streams.filter(only_audio=True).order_by('abr').desc():
+            bytes_per_second = stream.abr
+            if bytes_per_second not in audios:
+                audios.append(bytes_per_second)
+        return audios
+
+    def get_info(self):
+        """
+        print video info: title, publish date, description, duration,
+        available resolution and audio bit rate for download
+
+        :return: None
+        """
+        print("title: ", self.yt.title)
+        print("publish date: ", self.yt.publish_date)
+        print("description:")
+        print(self.yt.description)
+        print("length: ", readable_time(self.yt.length))
+        print("available resolution for download: ", self.get_all_resolution())
+        print("available audio quality for download: ", self.get_all_audio_quality())
+
+
+def readable_time(seconds):
+    """
+    convert seconds to hours:minutes:seconds, this assumes not extremely large number
+
+    :param seconds: number of seconds
+    :type seconds: int
+    :return: time in format hours:minutes:seconds
+    :rtype: str
+    """
+    hours = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return "{0:d}:{1:02d}:{2:02d}".format(hours, minutes, seconds) if hours > 0 else "{0:02d}:{1:02d}".format(minutes,
+                                                                                                              seconds)
